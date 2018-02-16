ebfd1711e709673a1147818a4633906ffd088633
Yan Galli <yangalli@cjr.org.br>
2018-02-16 15:34:20 -0200

    finalizada a parte de listar os contatos

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,4 +1,6 @@
-import { MODIFICA_ADICIONA_CONTATO_EMAIL, ADICIONA_CONTATO_ERRO, ADICIONA_CONTATO_SUCESSO } from './types';
+import { MODIFICA_ADICIONA_CONTATO_EMAIL, ADICIONA_CONTATO_ERRO, 
+  ADICIONA_CONTATO_SUCESSO, LISTA_CONTATO_USUARIO
+ } from './types';
 import b64 from 'base-64';
 import firebase from 'firebase';
 import _ from 'lodash';
@@ -67,3 +69,18 @@ export const habilitaInclusaoContato = () => (
     payload: false
   }
 )
+
+export const contatosUsuarioFetch = () => {
+  
+  const { currentUser } = firebase.auth();
+
+  return(dispatch) => {
+    let emailUsuarioB64 = b64.encode(currentUser.email);
+
+    firebase.database().ref(`/usuario_contatos/${emailUsuarioB64}`)
+      .on('value', snapshot => {
+        dispatch({ type: LISTA_CONTATO_USUARIO, payload: snapshot.val() })
+      })
+
+  }
+}
--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -1,8 +1,57 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ListView } from 'react-native';
+import { connect } from 'react-redux';
+import { contatosUsuarioFetch } from '../actions/AppActions';
+import _ from 'lodash';
 
-export default props => (
-  <View>
-    <Text>Contatos</Text>
-  </View>
-)
+
+class Contatos extends React.Component {
+  
+  componentWillMount() {
+    this.props.contatosUsuarioFetch();
+    // cria um array vazio, que será mandado para o dataSource
+    this.criaFonteDeDados(this.props.contatos);
+  }
+
+  // é executado sempre que há alteração das propriedades -> é executado só após a renderização dos componentes 
+  componentWillReceiveProps(nextProps) {
+    // atualiza as propriedades de contatos
+    this.criaFonteDeDados(nextProps.contatos);
+  }
+  
+  criaFonteDeDados(contatos) {
+    // é criado um datasource para verificar as mudancas dos registro -> começa vazio
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+
+    // recebe o array vazio contatos
+    this.fonteDeDados = ds.cloneWithRows(contatos);
+  }
+
+
+  render() {
+    return(
+      <ListView
+        enableEmptySections
+        dataSource={this.fonteDeDados}
+        // recebe os dados contidos no dataSource do cloneWithRows
+        renderRow={ data => (
+            <View style={{ flex: 1, padding: 20, borderBottomWidth: 1, borderColor: '#CCC' }} >
+              <Text style={{ fontSize: 25 }} >{data.nome}</Text>
+              <Text style={{ fontSize: 18 }}>{data.email}</Text>
+            </View>
+          )
+        }
+      />
+    );
+  }
+}
+
+mapStateToProps = state => {
+
+  const contatos = _.map(state.ListaContatosReducer, (val, uid) => {
+    return{ ...val, uid }
+  })
+  return { contatos }
+}
+
+export default connect(mapStateToProps, { contatosUsuarioFetch })(Contatos);
